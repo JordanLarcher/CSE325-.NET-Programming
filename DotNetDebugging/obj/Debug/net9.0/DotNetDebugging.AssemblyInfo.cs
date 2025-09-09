3a0b21e487ccbce56e9c3c3686f537699e22ae9c
jordan larcher <jordan.larcher.lds@gmail.com>
2025-09-09 19:59:23 -0300

    updating contoso pizza

--- DotNetDebugging/obj/Debug/net9.0/DotNetDebugging.AssemblyInfo.cs
+++ DotNetDebugging/obj/Debug/net9.0/DotNetDebugging.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetDebugging")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6397e865cbadd8c5f88b79787ec61d2c195c79d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetDebugging")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetDebugging")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
