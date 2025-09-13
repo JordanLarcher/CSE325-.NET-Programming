0b44665e1866a80f0b34a7a4b63532e79ad1d0ce
jordan larcher <jordan.larcher.lds@gmail.com>
2025-09-13 03:04:07 -0300

    updating bootstrap

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90e648a3e5e076fa32d8809e08f159bae5474cba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0df2956d8bc72e76f7fa3459602715ba63b8326e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
